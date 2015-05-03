38c711adfc6bbf7f9f928c4614abc9fef56d1bb1
Lucas Martins <lucasilvamartins@gmail.com>
2015-05-03 19:09:28 +0100

    First Commit

--- Httprequest/ViewController.swift
+++ Httprequest/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var authorNameField: UITextField!
+  @IBOutlet var searchButton: UIButton!
+  @IBOutlet weak var showResult: UILabel!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var tableView: UITableView!
+
+  let api = OpenLibraryAPI()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func searchAuthor(sender : AnyObject) {
+    
+    if let authorNameText = authorNameField.text {
+      if authorNameText != "" {
+        activityIndicator.startAnimating()
+        
+        
+        let searchResult = api.loadAuthor(authorNameText){ (authorFound) -> Void in
+          
+          var authorBooks: [String] = []
+          for book in authorFound.books {
+            authorBooks.append(book.title)
+          }
+          
+          self.activityIndicator.stopAnimating()
+          self.showResult.text = "Number of books: \(authorFound.numBooks)"//\(authorBooks)"
+        }
+      } else {
+        self.showResult.text = "Author Name not provided"
+      }
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
-
